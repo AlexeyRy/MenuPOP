5236e5e97b030bb13767df4ffe04ebf2d68946bf
Slight <Slight299792@gmail.com>
2024-12-07 06:39:47 +0200

    Initial Commit

---
+++ LLRDandDApp/LLRDandDAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLRDandDAppApp.swift
+//  LLRDandDApp
+//
+//  Created by Alexey Chermnykh on 7.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct LLRDandDAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
